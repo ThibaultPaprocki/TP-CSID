898c92714630e9c672e9ab38bd396f31f89fcc50
Alexandre <alex.paprocki@gmail.com>
2020-11-05 10:01:07 +0100

    Ajout de tp-maven-mojo et maven HelloWorld

---
+++ tp-maven-mojo/helloWorld/src/main/java/plugin/example/HelloWorld.java
@@ -0,0 +1,10 @@
+package plugin.example;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+
+public class HelloWorld extends AbstractMojo {
+	public void execute() throws MojoExecutionException {
+		getLog().info("HelloWorldNiceWeatherIsntIt?");
+	}
+}
